14ca795d16630a583039d24b3542c017bc7b1232
Gaurav Bhandari <sbhandari1907@protonmail.com>
2023-05-16 00:00:47 +0530

    done

--- src/utils/getVaccinationData.ts
+++ src/utils/getVaccinationData.ts
@@ -4,7 +4,40 @@ export const getVaccinationData = async () => {
     const resp = await fetch("https://api.cowin.gov.in/api/v1/reports/v2/getPublicReports?state_id=&district_id=&date=2023-05-14");
     const data = await resp.json();
     const stateWiseData: StateData[] = data.getBeneficiariesGroupBy;
-    const ageWiseData: AgeWise = data.vaccinationByAge;
+    const ageWiseResp:AgeWise = data.vaccinationByAge;
+    
+    const ageWiseData = [
+        {
+            title: "12-14",
+            value: ageWiseResp["vac_12_14"],
+            color:"#1b096c"
+        },
+        {
+            title: "15-17",
+            value: ageWiseResp["vac_15_17"],
+            color:"#3b15c2"
+        },
+        {
+            title: "18-45",
+            value: ageWiseResp["vac_18_45"],
+            
+            color:"#32139f"
+            
+        },
+        {
+            title: "45-60",
+            value: ageWiseResp["vac_45_60"],
+            color:"#4719e8"
+            
+        },
+        {
+            title: "above_60",
+            value: ageWiseResp["above_60"],
+            color:"#542bfc"
+        },
+    ]
+    
+
     const registrationData = data.topBlock.registration;
     const sitesConductingVaccination = data.topBlock.sites;
     const vaccination = vaccinationData(data.topBlock.vaccination);
@@ -15,42 +48,40 @@ export const getVaccinationData = async () => {
 const vaccinationData = (Data: V) => {
     const genderWiseData = [
         {
-            category: "male",
+            title: "male",
             value: Data.male,
+            color:"#542bfc"
         },
         {
-            category: "female",
+            title: "female",
             value: Data.female,
+            color:"#4719e8"
         },
-        {
-            category: "others",
-            value: Data.others
-        }
-    ]
-    const vaccineWiseData = [
-        {
-            name: "covishield",
-            value: Data.covishield,
-        },
-        {
-            name: "covaxin",
-            value: Data.covaxin,
-        }, {
-            name: "sputnik",
-            value: Data.sputnik,
-        }, {
-            name: "zycov",
-            value: Data.zycov,
-        }, {
-            name: "corbevax",
-            value: Data.corbevax,
-        },
-        {
-            name: "covovax",
-            value: Data.covovax,
-        }
+        // {
+        //     title: "others",
+        //     value: Data.others,
+        //     color:"#8fe101"
+        // }
     ]
 
+    const vaccineWiseData = [
+        {
+            title: "covishield",
+            value: Data.covishield,
+            color:"#32139f"
+            
+        },
+        {
+            title: "covaxin",
+            value: Data.covaxin,
+            color:"#4619e8"
+        }, {
+            title: "corbevax",
+            value: Data.corbevax,
+            color:"#32139f"
+        },
+        
+    ]
     const doseWiseData = {
         tot_dose_1: Data.tot_dose_1,
         tot_dose_2: Data.tot_dose_2,
